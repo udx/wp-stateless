8b6594326cdbc1f0e5ab729e98a778049c6e0103
Anton Korotkov <antonkorotkoff@gmail.com>
2016-06-10 18:35:46 +0300

    version fix

--- lib/classes/class-bootstrap.php
+++ lib/classes/class-bootstrap.php
@@ -25,7 +25,7 @@ namespace wpCloud\StatelessMedia {
        * @property $version
        * @type {Object}
        */
-      public static $version = '1.7.3';
+      public static $version = '1.8.0';
 
       /**
        * Singleton Instance Reference.
