712ba668f0d2d5ec1f433fe41f48a9c20b8afa0d
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2017-08-09 10:46:08 +0600

    Issue #124 text update on splash screen

--- static/views/stateless_splash_screen.php
+++ static/views/stateless_splash_screen.php
@@ -5,8 +5,8 @@
                 <div class="row">
                     <div class="col-xs-12 text-center">
                         <div class="wpStateLess-welcome-text">
-                            <h1>All Media Files in One Place</h1>
-                            <p>Stores and serves WordPress media files directly from Google Cloud Storage.</p>
+                            <h1>WP-Stateless</h1>
+                            <p>Upload and serve your WordPress media files from Google Cloud Storage.</p>
                             <a class="btn btn-rounded btn-green" href="<?php echo ud_get_stateless_media()->get_settings_page_url('?page=stateless-setup&step=google-login');?>">Get Started Now</a>
                         </div>
                     </div>
@@ -19,19 +19,19 @@
                     <div class="col-md-4">
                         <div class="wpStateLess-s-feature text-center">
                             <img src="<?php echo ud_get_stateless_media()->path( 'static/images/ficon1.png', 'url'  ); ?>" alt="" />
-                            <p>Painlessly Upload Existing Media Library</p>
+                            <p>Media Uploaded to Google Cloud</p>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="wpStateLess-s-feature text-center">
                             <img src="<?php echo ud_get_stateless_media()->path( 'static/images/ficon2.png'); ?>" alt="" />
-                            <p>Control Google Files From the Media Library</p>
+                            <p>Google Cloud Serves Media</p>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="wpStateLess-s-feature text-center">
                             <img src="<?php echo ud_get_stateless_media()->path( 'static/images/ficon3.png'); ?>" alt="" />
-                            <p>Delightful and Easy to Use Settings Screen</p>
+                            <p>Managed by WP-Stateless</p>
                         </div>
                     </div>
                 </div>
