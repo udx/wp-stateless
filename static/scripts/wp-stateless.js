3e7e7b6285a964b3beb41995e2130d71e033b762
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2018-01-31 21:02:48 -0600

    Bugfix

--- static/scripts/wp-stateless.js
+++ static/scripts/wp-stateless.js
@@ -656,6 +656,6 @@ var wpStatelessApp = angular.module('wpStatelessApp', [])
   $scope.sm.generatePreviewUrl();
 
 })
-.controller('wpStatelessSettings', function($scope, $filter) {
+.controller('wpStatelessCompatibility', function($scope, $filter) {
   $scope.modules = wp_stateless_compatibility || {};
 });
