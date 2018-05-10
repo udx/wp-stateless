84a5c48dfe6d09e6876c659fe9d6e2afa7db94bb
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2018-05-10 12:31:57 +0600

    Updated Readme file and plugin version

--- wp-stateless-media.php
+++ wp-stateless-media.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://www.usabilitydynamics.com
  * Description: Upload and serve your WordPress media files from Google Cloud Storage.
  * Author: Usability Dynamics, Inc.
- * Version: 2.1.4
+ * Version: 2.1.5-RC2
  * Text Domain: stateless-media
  * Author URI: https://www.usabilitydynamics.com
  *
