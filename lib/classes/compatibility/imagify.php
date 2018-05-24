655f3bf69436a409c9ca184d5f9dbd2a413bcfe7
Md. Alimuzzaman Alim <alim.alimuzzaman@usabilitydynamics.com>
2018-05-24 22:24:45 +0600

    Issue #245

--- lib/classes/compatibility/imagify.php
+++ lib/classes/compatibility/imagify.php
@@ -23,7 +23,8 @@ namespace wpCloud\StatelessMedia {
                 // We need to remove the regular handler for sync 
                 // unless in stateless mode we would remove the attachment before it's get optimized.
                 remove_filter( 'wp_update_attachment_metadata', array( "wpCloud\StatelessMedia\Utility", 'add_media' ), 999 );
-                add_filter( 'wp_update_attachment_metadata', array( $this, 'add_media_wrapper' ), 999, 2 );
+                // we are not using this for now untill we have queue system. Because if we use this add media button return local url.
+                // add_filter( 'wp_update_attachment_metadata', array( $this, 'add_media_wrapper' ), 999, 2 );
 
                 add_filter( 'before_imagify_optimize_attachment', array($this, 'fix_missing_file'), 10);
                 add_action( 'after_imagify_optimize_attachment', array($this, 'after_imagify_optimize_attachment'), 10 );
@@ -36,15 +37,27 @@ namespace wpCloud\StatelessMedia {
             }
 
             /**
+             * Not using for now.
              * Replacement for default wp_update_attachment_metadata filter of bootstrap class.
              * To avoid sync same image twice, once on upload and again after optimization.
              * We also avoid downloading image before optimization on stateless mode.
              */
             public function add_media_wrapper($metadata, $attachment_id){
                 $imagify = new \Imagify_Attachment($attachment_id);
-                if ( ! $imagify->is_extension_supported() ) {
+
+                if ( is_callable( array( $imagify, 'is_extension_supported' ) ) ) {
+                    if ( ! $imagify->is_extension_supported() ) {
+                        return ud_get_stateless_media()->add_media( $metadata, $attachment_id );
+                    }
+                } elseif ( function_exists( 'imagify_is_attachment_mime_type_supported' ) ) {
+                    // Use `imagify_is_attachment_mime_type_supported( $attachment_id )`.
+                    if ( ! imagify_is_attachment_mime_type_supported( $attachment_id ) ) {
+                        return ud_get_stateless_media()->add_media( $metadata, $attachment_id );
+                    }
+                } elseif(!wp_attachment_is_image($attachment_id)){
                     return ud_get_stateless_media()->add_media( $metadata, $attachment_id );
                 }
+
                 return $metadata;
             }
 
@@ -59,7 +72,7 @@ namespace wpCloud\StatelessMedia {
                 /**
                  * If hook is triggered by ShortPixel
                  */
-                if ( !$this->hook_from_imagify() ) return;
+                if ( !$this->hook_from_imagify($attachment_id) ) return;
 
                 /**
                  * If mode is stateless then we change it to cdn in order images not being deleted before optimization
@@ -71,20 +84,21 @@ namespace wpCloud\StatelessMedia {
                     $wp_stateless_imagify_mode = 'stateless';
                 }
 
-                $upload_dir = wp_upload_dir();
+                $upload_basedir = wp_upload_dir();
+                $upload_basedir = trailingslashit( $upload_basedir[ 'basedir' ] );
                 $meta_data = wp_get_attachment_metadata( $attachment_id );
-                $file = $upload_dir[ 'basedir' ] . "/" . $meta_data['file'];
-
+                $file = $upload_basedir . $meta_data['file'];
+                
                 /**
                  * Try to get all missing files from GCS
                  */
                 if ( !file_exists( $file ) ) {
-                    ud_get_stateless_media()->get_client()->get_media( apply_filters( 'wp_stateless_file_name', str_replace( trailingslashit( $upload_dir[ 'basedir' ] ), '', $file )), true, $file );
+                    ud_get_stateless_media()->get_client()->get_media( apply_filters( 'wp_stateless_file_name', $meta_data['file']), true, $file );
                 }
 
                 if ( !empty( $meta_data['sizes'] ) && is_array( $meta_data['sizes'] ) ) {
                     foreach( $meta_data['sizes'] as $image ) {
-                        if ( !empty( $image['gs_name'] ) && !file_exists( $file = trailingslashit( $upload_dir[ 'basedir' ] ).$image['gs_name'] ) ) {
+                        if ( !empty( $image['gs_name'] ) && !file_exists( $file = $upload_basedir . $image['gs_name'] ) ) {
                             ud_get_stateless_media()->get_client()->get_media( apply_filters( 'wp_stateless_file_name', $image['gs_name']), true, $file );
                         }
                     }
@@ -98,15 +112,13 @@ namespace wpCloud\StatelessMedia {
              *
              * @return bool
              */
-            private function hook_from_imagify() {
-                $call_stack = debug_backtrace();
-
-                if ( !empty( $call_stack ) && is_array( $call_stack ) ) {
-                    foreach( $call_stack as $step ) {
-                        if ( $step['function'] == 'optimize' && $step['class'] == 'Imagify_Attachment' ) {
-                            return true;
-                        }
-                    }
+            private function hook_from_imagify($attachment_id) {
+                $imagify = new \Imagify_Attachment($attachment_id);
+                if(method_exists($imagify, 'is_running')){
+                    return $imagify->is_running($attachment_id);
+                }
+                elseif(get_transient( 'imagify-async-in-progress-' . $attachment_id ) !== false){
+                    return true;
                 }
 
                 return false;
@@ -155,7 +167,6 @@ namespace wpCloud\StatelessMedia {
                 $backup_path = get_imagify_attachment_backup_path( $file_path );
                 if(!file_exists($backup_path)){
                     $upload_dir = wp_upload_dir();
-                    $overwrite = apply_filters( 'imagify_backup_overwrite_backup', false, $file_path, $backup_path );
                     $name = str_replace(trailingslashit( $upload_dir[ 'basedir' ] ), '', $backup_path);
                     $name = apply_filters( 'wp_stateless_file_name', $name);
                     do_action( 'sm:sync::syncFile', $name, $backup_path, true);
