a80235e950ca8e479c361a59ef07ca1454a80178
Md. Alimuzzaman Alim <alim.alimuzzaman@usabilitydynamics.com>
2019-02-21 20:13:57 +0600

    Update divi.php
    Updated Divi compatibility description.

--- lib/classes/compatibility/divi.php
+++ lib/classes/compatibility/divi.php
@@ -3,7 +3,8 @@
  * Theme Name: Divi
  * Theme URI: https://www.elegantthemes.com/gallery/divi/
  *
- * Compatibility Description: Ensures compatibility with Divi theme.
+ * Compatibility Description: Ensures compatibility with Divi themes Builder Export addon.
+ * https://github.com/wpCloud/wp-stateless/issues/224
  *
  */
 
@@ -15,7 +16,7 @@ namespace wpCloud\StatelessMedia {
             protected $id = 'divi';
             protected $title = 'Divi';
             protected $constant = 'WP_STATELESS_COMPATIBILITY_DIVI';
-            protected $description = 'Ensures compatibility with Divi.';
+            protected $description = 'Ensures compatibility with Divi Builder Export.';
             protected $theme_name = 'Divi';
 
             public function module_init($sm){
