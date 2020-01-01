a22cacc634abfcd4c578bab081f93ce638f44b1c
Md. Alimuzzaman Alim <alim.alimuzzaman@usabilitydynamics.com>
2020-01-02 01:23:02 +0600

    Version bump 2.3.3

--- lib/classes/class-bootstrap.php
+++ lib/classes/class-bootstrap.php
@@ -25,7 +25,7 @@ namespace wpCloud\StatelessMedia {
        * @property $version
        * @type {Object}
        */
-      public static $version = '2.3.2';
+      public static $version = '2.3.3';
 
       /**
        * Singleton Instance Reference.
