25e6c1d2fce05b01a81491f6e500beea3b4185b2
Md. Alimuzzaman Alim <alim.alimuzzaman@usabilitydynamics.com>
2020-03-30 17:15:22 +0600

    WP Stateless Elementor - CSS files wrong MIME type #395

--- lib/classes/class-sync-non-media.php
+++ lib/classes/class-sync-non-media.php
@@ -80,7 +80,7 @@ namespace wpCloud\StatelessMedia {
                     return false;
                 }
 
-                $file_type = wp_check_filetype($absolutePath);
+                $file_type = Utility::mimetype_from_extension(pathinfo($absolutePath, PATHINFO_EXTENSION));
                 if(empty($this->client)){
                     $this->client = ud_get_stateless_media()->get_client();
                 }
@@ -112,7 +112,7 @@ namespace wpCloud\StatelessMedia {
                         'absolutePath' => $absolutePath,
                         'cacheControl' => apply_filters( 'sm:item:cacheControl', 'public, max-age=36000, must-revalidate', $absolutePath), //@todo use cacheControl from settings page.
                         'contentDisposition' => apply_filters( 'sm:item:contentDisposition', null, $absolutePath),
-                        'mimeType' => $file_type['type'],
+                        'mimeType' => $file_type,
                         'metadata' => array(
                             'child-of' => dirname($name),
                             'file-hash' => md5( $name ),
