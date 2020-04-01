b3b7ceac9957a2469743c6a1bb879e4d769a6172
vladimir <plan.vova@gmail.com>
2020-04-01 11:48:53 +0300

    #461 wildcards for mainsite

--- lib/classes/class-bootstrap.php
+++ lib/classes/class-bootstrap.php
@@ -1634,12 +1634,14 @@ namespace wpCloud\StatelessMedia {
            * https://github.com/wpCloud/wp-stateless/issues/461
            */
           if ( is_multisite() ) {
-            $blog_id = get_current_blog_id();
-            $root_dir_sm = trim($root_dir_sm, '/ ');
-            if( strpos($root_dir_sm, "sites/%site_id%/%date_year%/%date_month%") === 0 || strpos($root_dir_sm, "sites/%site_id%") === 0 ) {
-              $upload_data[ 'basedir' ] = $upload_data[ 'basedir' ] . "/sites/$blog_id";
-              $upload_data[ 'baseurl' ] = $upload_data[ 'baseurl' ] . "/sites/$blog_id";
-              $root_dir    = apply_filters("wp_stateless_handle_root_dir", str_replace('sites/%site_id%/', '', $root_dir_sm));
+            if ( ! is_main_site() ) {
+              $blog_id = get_current_blog_id();
+              $root_dir_sm = trim($root_dir_sm, '/ ');
+              if ( strpos($root_dir_sm, "sites/%site_id%/%date_year%/%date_month%") === 0 || strpos($root_dir_sm, "sites/%site_id%") === 0 ) {
+                $upload_data['basedir'] = $upload_data['basedir'] . "/sites/$blog_id";
+                $upload_data['baseurl'] = $upload_data['baseurl'] . "/sites/$blog_id";
+                $root_dir = apply_filters("wp_stateless_handle_root_dir", str_replace('sites/%site_id%/', '', $root_dir_sm));
+              }
             }
           }
           $upload_data[ 'subdir' ] = $upload_data[ 'subdir' ] . '/' . $root_dir;
