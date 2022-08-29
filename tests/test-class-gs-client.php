c621c549c470b97f44be30d8b53d4e4b08736eb5
Charley Sheets <rcsheets@picote.ch>
2022-08-28 22:50:50 -0700

    Add a trivial unit test (it always succeeds)

---
+++ tests/test-class-gs-client.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Class GS_ClientTest
+ *
+ * @package Wp_Stateless
+ */
+
+use wpCloud\StatelessMedia\GS_Client;
+
+class GS_ClientTest extends WP_UnitTestCase {
+	public function test_get_instance() {
+        // This should probably call get_instance() but it doesn't yet. At this point I just hope it runs.
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
